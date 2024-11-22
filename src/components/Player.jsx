03b079fad9f781df4459a973bb55621f9cedd447
richardborgesdev <richardborgesdev@gmail.com>
2024-11-22 15:55:04 -0300

    chore: change html element dynamically

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
+
 export function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick(params) {
+    setIsEditing(true);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+
+  if (isEditing) {
+    playerName = <input type="text" required />
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>Edit</button>
     </li>
   );
 }
